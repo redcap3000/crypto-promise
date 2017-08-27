db7a45ca2115667b533e8a3c0f11293de6f9c8bc
Ronaldo Barbachano <redcap3000@gmail.com>
2017-08-26 23:30:42 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,156 @@
+An implementation of promises to monitor cryptocurrency mining and 
+    exchange operations. Currently supports hitbtc, bittrex, and nanopool with a 
+    hacked version of nanopool_api npm module to support easy checking of 
+    multiple nanopool accounts for all supported
+    nanopool currencies. Outputs response (JSON) using express @ /user endpoint.
+
+##Nanopool address checker
+You can check many nanopool addresses by adding them to the accounts.json.
+
+Example response with one eth address and one sia address.
+
+**http://localhost:8080/nanopool**
+
+```
+[
+  [
+     //currency
+    "eth",
+    //last block found time from now
+    "20 minutes",
+    //total (balance+unconfirmed) amount
+    0.01766751,
+    {
+      "h1": "14.2",
+      "h3": "24.1",
+      "h6": "21.5",
+      "h12": "22.1",
+      "h24": "22.2"
+    },
+    // worker count, total rating (of all workers)
+    "Workers : 1 , rating : 1462, hashrate : 0.0"
+  ],
+  [
+    "sia",
+    "7 minutes",
+    57.92600031,
+    {
+      "h1": "215.0",
+      "h3": "143.3",
+      "h6": "164.8",
+      "h12": "179.2",
+      "h24": "179.2"
+    },
+    "Workers : 1 , rating : 1434, hashrate : 258.0"
+  ]
+]
+``` 
+
+##Exchange API Feeds
+
+The exchange feeds include wallet balances, orders, and ticker information and require working/valid API keys/secrets.
+
+The exchange feed example
+
+**http://localhost:8080/hitbtc**
+
+```
+
+[
+  [
+     // symbol name
+    "XRP",
+    // total available balance 
+    99,
+    // balance on orders
+    75,
+    // open orders (hitbtc shown, bittrex different)
+    [
+      {
+        "orderId": "2208561488",
+        "orderStatus": "new",
+        "lastTimestamp": 1503594663405,
+        "orderPrice": "0.00007082",
+        "orderQuantity": 15,
+        "avgPrice": "0",
+        "quantityLeaves": 15,
+        "type": "limit",
+        "timeInForce": "GTC",
+        "cumQuantity": 0,
+        "clientOrderId": "31ba57729f2b455e8078520587123376",
+        "symbol": "XRPBTC",
+        "side": "sell",
+        "execQuantity": 0
+      },
+      {
+        "orderId": "2262290628",
+        "orderStatus": "new",
+        "lastTimestamp": 1503718002212,
+        "orderPrice": "0.00005289",
+        "orderQuantity": 60,
+        "avgPrice": "0",
+        "quantityLeaves": 60,
+        "type": "limit",
+        "timeInForce": "GTC",
+        "cumQuantity": 0,
+        "clientOrderId": "dbd7ca5fb8e84fb4b39176e954a07ee8",
+        "symbol": "XRPBTC",
+        "side": "sell",
+        "execQuantity": 0
+      }
+    ],
+    // ticker's based on open orders
+    [
+      "XRPBTC",
+      {
+        "ask": "0.00004746",
+        "bid": "0.00004683",
+        "last": "0.00004707",
+        "low": "0.00004508",
+        "high": "0.00005078",
+        "open": "0.00005059",
+        "volume": "3158721",
+        "volume_quote": "152.00000000",
+        "timestamp": 1503817250112
+      }
+    ]
+  ],
+    
+    
+```
+###Quickstart
+1) Configure env variables:
+    
+```
+export HITBTC_API_KEY=KEY
+export HITBTC_API_SECRET=SECRET
+
+export BITTREX_API_KEY=KEY
+export BITTREX_API_SECRET=SECRET
+```
+2) Edit accounts.json (for nanopool)
+ 
+
+```
+{
+    "nanopool" :
+        {   "eth" :
+                [
+                    "ETH ADDRESSES"
+                ],
+            "sia" :
+                [
+                    "SIA ADDRESSES"
+                ]
+        }
+}
+```
+
+3) Point browser to endpoints:
+
+http://localhost:8080/nanopool
+
+http://localhost:8080/hitbtc
+
+http://localhost:8080/bittrex
+
---
+++ index.js
@@ -0,0 +1,327 @@
+/*
+    
+    crypto-promise
+
+    An implementation of promises to monitor cryptocurrency mining and 
+    exchange operations. Currently supports hitbtc, bittrex, and nanopool with a 
+    hacked version of nanopool_api npm module to support easy checking of 
+    multiple nanopool accounts for all supported
+    nanopool currencies. Outputs response (JSON) using express @ /user endpoint.
+
+    
+*/
+
+var Promise = require('promise');
+
+if (typeof process.env.HITBTC_API_KEY != 'undefined' && typeof process.env.HITBTC_API_SECRET != 'undefined') {
+    console.log('hitbtc key found')
+    HitBTC = require('hitbtc-api')
+}
+
+if (typeof process.env.BITTREX_API_KEY != 'undefined' && typeof process.env.BITTREX_API_SECRET != 'undefined') {
+    console.log("bittrex key found")
+
+    var bittrex = require('@you21979/bittrex.com')
+    var api = bittrex.PublicApi;
+    var errors = require('@you21979/bittrex.com/errors')
+    api = bittrex.createPrivateApi(process.env.BITTREX_API_KEY, process.env.BITTREX_API_SECRET, "none")
+}
+
+
+
+// https://stackoverflow.com/questions/3177836/how-to-format-time-since-xxx-e-g-4-minutes-ago-similar-to-stack-exchange-site
+function timeSince(date) {
+
+    var seconds = Math.floor((new Date() - date) / 1000);
+
+    var interval = Math.floor(seconds / 31536000);
+
+    if (interval > 1) {
+        return interval + " years";
+    }
+    interval = Math.floor(seconds / 2592000);
+    if (interval > 1) {
+        return interval + " months";
+    }
+    interval = Math.floor(seconds / 86400);
+    if (interval > 1) {
+        return interval + " days";
+    }
+    interval = Math.floor(seconds / 3600);
+    if (interval > 1) {
+        return interval + " hours";
+    }
+    interval = Math.floor(seconds / 60);
+    if (interval > 1) {
+        return interval + " minutes";
+    }
+    return Math.floor(seconds) + " seconds";
+}
+nanoapi = require('nanopool_api');
+try {
+    var accounts = require('./accounts.json')
+} catch (error) {
+    console.log("Problem reading accounts.json")
+    //console.log(error)
+    //process.exit()
+}
+// setup express server
+
+const express = require('express')
+const app = express()
+var bodyParser = require("body-parser")
+
+app.use(bodyParser.json({ type: 'application/*+json' }))
+
+app.get('/bittrex',function(req,res){
+    let balances = []
+    api.getBalances().then((row,err)=>{
+    if(typeof err == 'undefined'){
+        row.filter((o)=>{
+            if(o.Balance === 0 && o.Available === 0 && o.Pending === 0){
+            }else{
+                let obj = {
+                    symbol : o.Currency,
+                    available : o.Available,
+                    pending : o.Pending,
+                    onOrders : o.Balance - o.Available
+                }   
+                    // then this symbol has open orders?
+                if(o.Balance - o.Available > 0){
+                    // look up  open orders
+                    balances.push(obj)
+                }
+            }
+
+        })
+
+        }
+        return balances
+    }
+    ).then((wallets)=>{
+        let openOrders=[]
+        let orderSymbols=[]
+        let finalResult=[]
+        let innerPromises = []
+        // get unique symbols?
+        wallets.filter((wallet,i)=>{
+            //console.log(wallet)
+            if(parseInt(orderSymbols.indexOf(wallet.symbol))=== -1){
+                //orderSymbols.push(wallet.symbol)
+                openOrders.push(api.getOpenOrders(wallet.symbol))
+            }
+            if(i === wallets.length-1){
+                //console.log('open orders length' + openOrders.length)
+             
+                let r = Promise.all(openOrders).then((values) => {
+                    if(values[0][0].OrderUuid === values[1][0].OrderUuid){
+                        // only parse the top row
+                        // weird azz thing going on here dunno why.... 
+                        values[0].filter((order)=>{
+                            innerPromises.push(
+                                bittrex.PublicApi.getTicker(order.Exchange)
+                                )
+                        })
+                        result = Promise.all(innerPromises).then((fValues)=>{
+                            var r = []
+                            wallets.filter((w,wI)=>{
+                                if(w.available === 0){
+                                    w.available = w.onOrders
+                                }
+                                r.push([
+                                        w.symbol,
+                                        parseFloat(w.available)+w.onOrders,
+                                        w.onOrders,
+                                        values[wI],
+                                        fValues[wI]
+                                        ]
+                                    )
+
+                            })
+                            res.send(r)
+                        })
+                    }else{
+                        console.log("This is a really strange error inside a hack to fix another error")
+                        process.exit()
+                    }
+                })
+            }
+        })
+    })
+})
+
+app.get('/hitbtc', function(req, res) {
+
+    makeHitBtcClient = function(key, secret) {
+        try {
+            restClient = new HitBTC.default({ key, secret, isDemo: false })
+        } catch (error) {
+            console.log("Could not make hitbtc client.")
+            console.log(error)
+            return false
+        }
+        return true
+    }
+
+    getHitbtcBalance = function(key, secret) {
+        makeHitBtcClient(key, secret)
+        let { getMyBalance } = restClient
+        // return a promise
+        return getMyBalance()
+    }
+
+    hitbtcCurrencies = []
+
+    getHitbtcBalance(process.env.HITBTC_API_KEY, process.env.HITBTC_API_SECRET).then((balance) => {
+        // balances
+        let b = []
+        let bal = balance.balance
+        for (var key in bal) {
+            if (bal[key].cash > 0 || bal[key].reserved > 0) {
+                b.push([
+                    bal[key].currency_code,
+                    // show how much total!
+                    bal[key].cash + bal[key].reserved, bal[key].reserved
+                ])
+            }
+        }
+        return b;
+    }).then((wallets) => {
+        let { getMyActiveOrders } = restClient
+        let openOrders = []
+        let symbols = []
+        let orderDict = {}
+        return getMyActiveOrders().then((orders) => {
+
+            orders.orders.filter((order, z) => {
+                if (symbols.indexOf(order.symbol) == -1) {
+                    symbols.push(order.symbol)
+                    openOrders[order.symbol] = [order]
+                } else {
+                    openOrders[order.symbol].push(order)
+                }
+            })
+            wallets.filter((wallet, i) => {
+                symbols.filter(function(symbol) {
+                    if (symbol.startsWith(wallet[0])) {
+                        // look up quote!
+                        wallets[i].push(openOrders[symbol])
+                    }
+                })
+            })
+            // group open orders with
+            return wallets
+        })
+    }).then((wallets) => {
+        // determine wallets with orders so we know when to send a response back ... :/
+        // the promises-friendly alternative is here:
+        // https://stackoverflow.com/questions/33355528/filtering-an-array-with-a-function-that-returns-a-promise
+        walletsWithOrderCount = 0
+        orderIndex = {}
+        wallets.filter((w,i)=>{
+            if(typeof w[3] != 'undefined' && w[3].length > 0){
+                if(typeof orderIndex[w[0]] == 'undefined'){
+                    walletsWithOrderCount += 1
+                    orderIndex[w[0]] = true
+                }else{
+                    console.log("Duplicate symbol within an order")
+                    // no need to recount?
+                }
+            }
+        })
+        // also will want to check for symbols that have orders in multiple markets
+        // i.e. if theres an order for ETHBTC and BCCETH
+        // TO DO!
+
+        wallets.filter((wallet, i) => {
+            let resolution = []
+            let activeSymbols = []
+            if (typeof wallet[3] != 'undefined') {
+                let orders = wallet[3]
+                orders.filter((o, n) => {
+                    //console.log(o.symbol)
+                    if (activeSymbols.indexOf(o.symbol) == -1) {
+                        activeSymbols.push(o.symbol)
+                    }
+                })
+                activeSymbols.filter((sym) => {
+                    let { getTicker } = restClient
+                    resolution.push(getTicker(sym))
+                })
+                results = []
+                Promise.all(resolution).then(values => {
+                    if (activeSymbols.length === values.length) {
+                        if (activeSymbols.length === 1) {
+                            wallet.push([activeSymbols[0], values[0]])
+                        } else {
+                            wallet.push([activeSymbols, values])
+                        }
+                        // res send here .. :( ) 
+                        results.push(wallet)
+                        if(results.length === walletsWithOrderCount){
+                            res.send(results)
+                        }else{
+                            //console.log(results.length-1 + ' > ' + wallets.length + ' : ' + i)
+                        }
+                    }
+                })
+            }
+
+        })
+    })
+
+})
+app.get('/nanopool', function(req, res) {
+    let response = [],
+        supportedCurrencies = [],
+        r = [],
+        nanoCallback = function(response, json) {
+            if (typeof json != 'undefined' && typeof json.status != 'undefined' && json.status) {
+                fulfill(json.data)
+            }
+            reject(response)
+        };
+
+    if (accounts.nanopool != 'undefined') {
+        for (var currency in accounts.nanopool) {
+            accounts.nanopool[currency].filter(function(address) {
+                let iCurr = currency,
+                    rating = 0
+
+                r.push(new Promise(function(fulfill, reject) {
+                    nanoapi.user(currency, address, function(response, json) {
+                        if (typeof json != 'undefined' && typeof json.status != 'undefined' && json.status) {
+                            if (json.data.workers.length > 0) {
+                                // last share completion (time from now value)
+                                var lastShare = timeSince(new Date(json.data.workers[0].lastShare * 1000))
+                                if (json.data.workers.length > 0) {
+                                    // adding ratings of all workers
+                                    json.data.workers.filter(function(worker) {
+                                        rating += worker.rating
+                                    })
+                                }
+                            } else {
+                                var lastShare = 0
+                            }
+                            fulfill([ iCurr,lastShare, parseFloat(json.data.balance) + parseFloat(json.data.unconfirmed_balance), json.data.avgHashrate,'Workers : ' + json.data.workers.length + ' , rating : ' + rating + ', hashrate : ' + json.data.hashrate,])
+                        } else {
+                            reject(response)
+                        }
+                    })
+                }))
+            })
+        }
+    }
+
+    Promise.all(r).then(values => {
+        res.setHeader('Content-Type', 'application/json')
+        res.send(JSON.stringify(values))
+    }).catch(err => {
+        console.log(err.body)
+        res.send(err.body)
+    })
+})
+
+app.listen(process.env.PORT || 8080, function() {
+    console.log('Nanopool wallet tracker app listening on port ' + (process.env.PORT ? process.env.PORT : 8080))
+})
